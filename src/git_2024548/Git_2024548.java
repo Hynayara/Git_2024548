03f552e4189b7030e7fb4d47b8da1368a084d934
Hynayara <hynayarahfo@gmail.com>
2024-12-25 13:50:10 -0500

    Homework-Git Test (3%)
    added a simple print

--- src/git_2024548/Git_2024548.java
+++ src/git_2024548/Git_2024548.java
@@ -1,4 +1,4 @@
-/*
+/* 
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
@@ -14,7 +14,7 @@ public class Git_2024548 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println ("Hyna - 2024548");
         // TODO code application logic here
     }
-    
-}
+ }
